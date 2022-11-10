ae51fa6b60e1ba308c2048cabb0b8840bcaae896
Zephyre3777 <davidsonmaza1@gmail.com>
2022-11-10 09:21:37 -0600

    made it so that you can compare Rock to computer and get valid input but doesn't work when you try and compare paper to anything other then than a win, and scizzors is completly  bugged

--- rpc.js
+++ rpc.js
@@ -1,8 +1,8 @@
 //computer makes the choice
-
+let computerChoice
 function getComputerChoice(){
 //random number generated is made and translated to rock paper or sizors
-let computerChoice=Math.random() * 100
+computerChoice=Math.random() * 100
 console.log(computerChoice)
 if (computerChoice <= 33)
 {computerChoice="Rock"}
@@ -11,17 +11,18 @@ else if(computerChoice <= 66)
 {computerChoice="Paper"}
 
 else if(computerChoice <= 99)
-{computerChoice = "Sizorzzs"}
+{computerChoice = "Scizzors"}
 return computerChoice
 }
-console.log(getComputerChoice())
+console.log("computer choice"+getComputerChoice())
 
 console.log("TesT")
-console.log(Math.random()*100)
-//Player is asked to make choice 
-function getplayerselection(playerselection){
+
+//Player is asked to make choice
+let playerselection 
+function getplayerselection(){
     //player enters choice
-    playerselection=prompt("Rock, Paper, Sizzors shoot")
+    playerselection=prompt("Rock, Paper, Scizzors shoot")
     if (playerselection==="Rock"||
         playerselection==="Paper"||
         playerselection==="Scizzors")
@@ -30,21 +31,39 @@ function getplayerselection(playerselection){
     //choice is checked to makes sure it is valid
     //return playerselection
 }
-console.log(getplayerselection())
+console.log("player choice"+getplayerselection())
 
+
+//choices are compared and evaluated
 function game(){
     if (playerselection==="Rock")
-    {return(playerselection+ computerChoice)}
+    {if (computerChoice=== "Rock"){return playerselection+" VS "+computerChoice+" Thats a Tie"}
+    else if (computerChoice=== "Paper"){return playerselection+" VS "+computerChoice+" Rock looses to Paper, YOU LOST!!!"}    
+    else if (computerChoice=== "Scizzors"){return playerselection+" VS "+computerChoice+" Rock beats Scizzors, YOU WIN!!!"}
+}
+
     else if (playerselection==="Paper")
-    {}
+    {
+        if  (computerChoice=== "Rock"){return playerselection+ " VS "+computerChoice+" Paper beats Rock, YOU WIN!!!"}
+        else if (computerChoice=== "Paper"){playerselection+ " VS "+computerChoice+" Thats a tie"}
+        else if (computerChoice=== "Scizzors"){playerselection+ " VS "+computerChoice+" Paper looses to scizzors, YOU LOST!!!"}
+    }
     else if (playerselection==="Scizzors")
-    {}
+    {
+        if  (computerChoice=== "Rock"){playerselection+ " VS "+computerChoice+" Scizzors looses to Rock, YOU LOSE!!!"}
+        else if (computerChoice=== "Paper"){playerselection+ " VS "+computerChoice+" Sizzorse beats Paper, YOU WIN!!!"}
+        else if (computerChoice=== "Scizzors"){playerselection+ " VS "+computerChoice+ "Thats a tie"}
+    }
 }
 console.log(game())
 
 
 
-
+/*        if  (computerChoice=== "Rock"){}
+        else if (computerChoice=== "Paper"){}
+        else if (computerChoice=== "Scizzors"){}
+        if  (computerChoice=== "Rock"){playerselection+ " VS "+computerChoice}
+        else if (computerChoice=== "Paper"){playerselection+ " VS "+computerChoice}
 /*
 Computer makes there choice
 
